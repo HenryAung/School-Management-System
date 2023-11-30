9a3e36585e3f923134515768680e6f442b46d2a6
Henry Aung <hanelattaung7@gmail.com>
2023-11-30 11:27:34 +0000

    home page created with side menu and nav bar

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
-
-const app = express();
-
+const app = express(); 
+const path = require("path"); 
 const mysql = require('mysql2'); // Import the mysql library
+const dotenv = require('dotenv'); 
+
+dotenv.config({path: './.env'});  
 
 const PORT = 3000;
 
 // Create a MySQL connection pool
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'hla1929@mysql',
-  database: 'schoolmanagement'
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
 });
+
+
 
 connection.connect(function(err) {
   if (err) {
@@ -22,8 +26,14 @@ connection.connect(function(err) {
   console.log('Connected to the MySQL server.');
 });
 
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory)); 
+
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
-  res.send('Successful response.');
+  res.render('index');
 });
 
 app.listen(PORT, () => console.log('Example app is listening on port 3000.'));
