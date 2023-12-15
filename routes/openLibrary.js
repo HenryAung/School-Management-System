38d3b9fdabd8af84c2283df92ff83b93165f7b77
Henry Aung <hanelattaung7@gmail.com>
2023-12-15 07:17:19 +0000

    open library is implemented

--- routes/openLibrary.js
+++ routes/openLibrary.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const openLibControllers = require('../controllers/openLibrary')
+const axios = require('axios')
 
   
-router.get('/', openLibControllers.getISBN);
-router.post('/', openLibControllers.postISBN)
+router.get('/', async (req, res) => {
+    try {
+        const query = req.query.search;
+        console.log("my query is " + query)
+        const url = `https://openlibrary.org/search.json?q=${query}`;
+        const response = await axios.get(url);
+        console.log("my data is " + response.data.docs)
+        res.render('openlibrary', { books: response.data.docs , error : '' });
+    } catch (error) {
+        console.error(error);
+        res.render('openLibrary', { books: null, error: 'Error retrieving data' });
+    }
+});
 
 
 module.exports = router;
